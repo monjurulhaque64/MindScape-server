79a4634b70b34d079a71b0134149f601740f8116
monjurulhaque64 <monjurulhoque64@gmail.com>
2023-06-15 01:33:09 +0600

    enroll edit

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1cvgdrp.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -54,6 +54,13 @@ async function run() {
       console.log(item);
       const result = await enrollCollection.insertOne(item);
       res.send(result);
+    })
+
+    app.delete('/enrolls/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await enrollCollection.deleteOne(query)
+      res.send(result)
     })
 
 
